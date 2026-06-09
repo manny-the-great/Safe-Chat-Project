b35562916d6db8b8b4a8375dfcc8ac11bb63a175
Emmanuel Johnson <leunammejohnson17@gmail.com>
2026-06-09 11:35:52 +0100

    fix: add auth dependency to users initial migration

--- backend/users/migrations/0001_initial.py
+++ backend/users/migrations/0001_initial.py
@@ -1,8 +1,10 @@
-﻿from django.db import migrations, models
+from django.db import migrations, models
 
 class Migration(migrations.Migration):
     initial = True
-    dependencies = []
+    dependencies = [
+        ('auth', '0012_alter_user_first_name_max_length'),
+    ]
     operations = [
         migrations.CreateModel(
             name='User',
